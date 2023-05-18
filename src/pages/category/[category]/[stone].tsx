56b9d4fb8dc8646d094d7a45f8c6dedea79c3e61
Narek Hovhannisyan <n.hovo@bk.ru>
2023-05-18 15:12:39 +0400

    created category page

---
+++ src/pages/category/[category]/[stone].tsx
@@ -0,0 +1,12 @@
+import Layout from '@/components/features/Layout';
+import StonePage from '@/components/pages/StonePage';
+
+const Category = () => {
+  return (
+    <Layout>
+      <StonePage />
+    </Layout>
+  );
+};
+
+export default Category;
