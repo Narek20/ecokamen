86e2d0f50fa5129f418a3e164a7c100b7f41f2ce
Narek Hovhannisyan <n.hovo@bk.ru>
2023-05-30 23:00:59 +0400

    fixed minor bugs and added searchbar logic

--- src/components/features/SearchBar/index.tsx
+++ src/components/features/SearchBar/index.tsx
@@ -1,14 +1,67 @@
-import { Box, TextField, IconButton } from '@mui/material';
+import {
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
 import SearchIcon from '@mui/icons-material/Search';
-import logo from '../../../assets/svg/logo.svg';
+import { Box, TextField, IconButton, MenuItem } from '@mui/material';
+import { getStonesBySearchKey } from '@/services/stone.service';
+import logo from '@/assets/svg/logo.svg';
+import Basket from '../Basket';
+import { IStone } from '@/types/stone.types';
 
 import styles from './styles.module.scss';
-import Basket from '../Basket';
-import { useRouter } from 'next/router';
-import Image from 'next/image';
-import Link from 'next/link';
 
 const SearchBar = () => {
+  const [searchItems, setSearchItems] = useState<IStone[] | []>([]);
+  const [isFocused, setIsFocused] = useState(false);
+  const [searchKey, setSearchKey] = useState('');
+
+  const textFieldRef = useRef<HTMLDivElement>(null);
+
+  const getSearchItems = async (searchKey: string) => {
+    const data = await getStonesBySearchKey(searchKey);
+
+    if (data.success) {
+      setSearchItems(data.data);
+    }
+  };
+
+  useEffect(() => {
+    if (textFieldRef.current) {
+      const handleFocus = (evt: Event) => {
+        evt.stopPropagation()
+        evt.preventDefault()
+        setIsFocused(true);
+      };
+      
+      const handleBlur = () => {
+        setIsFocused(false);
+      };
+      document.body.addEventListener("click", handleBlur)
+
+      const inputElement = textFieldRef.current.querySelector('input');
+
+      if (inputElement) {
+        inputElement.addEventListener('click', handleFocus);
+      }
+
+      return () => {
+        if (inputElement) {
+          inputElement.removeEventListener('focus', handleFocus);
+          inputElement.removeEventListener('blur', handleBlur);
+        }
+      };
+    }
+  }, []);
+
+  useEffect(() => {
+    if (searchKey) getSearchItems(searchKey);
+    else getSearchItems('allStones');
+  }, [searchKey]);
+
   return (
     <Box className={styles.searchContainer}>
       <Box className={styles.logoContainer}>
@@ -20,6 +73,8 @@ const SearchBar = () => {
         <TextField
           label="Поиск"
           className={styles.search}
+          ref={textFieldRef}
+          autoComplete="off"
           InputProps={{
             endAdornment: (
               <IconButton>
@@ -27,7 +82,20 @@ const SearchBar = () => {
               </IconButton>
             ),
           }}
+          onChange={(evt) => setSearchKey(evt.target.value)}
         />
+        {isFocused &&
+          searchItems.map((searchItem: IStone) => (
+            <Link href={"/category/" + searchItem.link} >
+              <MenuItem
+                className={styles.menuItem}
+                key={searchItem.title}
+                value={searchItem.title}
+              >
+                {searchItem.title}
+              </MenuItem>
+            </Link>
+          ))}
       </Box>
       <Basket />
     </Box>
